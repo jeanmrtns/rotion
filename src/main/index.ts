d07eab4b79753a048e86696ca8bfcfd0f5ae39cf
Jean Martins <jeanmrtns4@gmail.com>
2023-04-21 23:11:09 -0300

    Application base layout

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,6 +10,11 @@ function createWindow(): void {
     height: 670,
     show: false,
     backgroundColor: '#17141f',
+    titleBarStyle: 'hiddenInset',
+    trafficLightPosition: {
+      x: 20,
+      y: 20,
+    },
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,17 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
 export function App() {
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100">
-      <h1>Hello Rotion</h1>
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
     </div>
   )
 }
